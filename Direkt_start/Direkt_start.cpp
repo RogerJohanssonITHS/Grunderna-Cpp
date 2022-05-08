0c7186c52f81c971a226480e12d165430566d9d5
RogerJohanssonITHS <89128014+RogerJohanssonITHS@users.noreply.github.com>
2022-05-08 22:53:14 +0200

    Starting.

--- Direkt_start/Direkt_start.cpp
+++ Direkt_start/Direkt_start.cpp
@@ -2,19 +2,17 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    int dag_antal, dagspris, tot_pris;
+    const int skatt = 10;
+    double kurs;
+    cout << "Ange antal dagar och pris per dag: ";
+    cin >> dag_antal >> dagspris;
+    cout << "Växlingskurs? ";
+    cin >> kurs;
+    tot_pris = skatt + dag_antal * dagspris;
+    cout << "Totalt pri: " << tot_pris << " euro"
+        << " (" << tot_pris * kurs << " kr)" << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
