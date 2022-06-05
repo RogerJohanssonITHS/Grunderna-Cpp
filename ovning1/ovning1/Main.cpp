5ca1114a60cc729525a24edc13c1418d2fd67c3f
RogerJohanssonITHS <89128014+RogerJohanssonITHS@users.noreply.github.com>
2022-06-05 23:53:59 +0200

    1 completed.

---
+++ ovning1/ovning1/Main.cpp
@@ -0,0 +1,11 @@
+// Konstruera ett program som skriver ut teckensträngen "Hello world" på skärmen. Först ska
+// programmet generera 2 blankrader, därefter teckensträngen och till sist 3
+// blankrader.Kontrollera antalet utskrivna rader noga.
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "\n" << "\n" << "Hello world!\n" << "\n" << "\n" << "\n";
+	return 0;
+}
