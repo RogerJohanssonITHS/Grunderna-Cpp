78045898c9d3d8c33b962fa6422cb52459a4f260
RogerJohanssonITHS <89128014+RogerJohanssonITHS@users.noreply.github.com>
2022-06-06 21:00:20 +0200

    ovning 2 started. Inluppgift started.

---
+++ ovning 2/ovning 2/Main.cpp
@@ -0,0 +1,3 @@
+// Konstruera ett program som läser in ett tal till en heltalsvariabel och sedan skriver ut talet
+// på skärmen, med den inledande texten : "Talet du matade in var X".Där X motsvarar det
+// inmatade talet.Prova att ge ett realtal som 12.345 till programmet.Vad händer ?
